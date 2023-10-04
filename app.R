14ba5e8aabb7855974e4701effdc18cf2d804681
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-04 23:33:29 +0500

    finalized the app code

--- app.R
+++ app.R
@@ -40,21 +40,28 @@ server <- function(input, output) {
     cocaine_data %>%
       group_by(year) %>%
       summarise(average_price = mean(switch(input$price, Retail = retail_price, Wholesale = wholesale_price)))
-    
   })
   
   # Generating the line chart plot
   output$line_chart <- renderPlotly({
     req(input$drug, input$country)
     
-    # Custom hover info
-    hover_info <- paste("<b>Year:", filtered_data()$year, "</b><br>",
-                        ifelse(input$price == "Retail", "<b>Retail Price: </b>", "<b>Wholesale Price: </b>"), 
-                        sprintf("<b>%.2f</b>", ifelse(input$price == "Retail", filtered_data()$retail_price, filtered_data()$wholesale_price)))
+    hover_info <- lapply(1:nrow(filtered_data()), function(i) {
+      year <- filtered_data()$year[i]
+      retail_price <- filtered_data()$retail_price[i]
+      overall_avg <- overall_average()$average_price[i]
+      paste(
+        "<b>Year:", year, "</b><br>",
+        "<b>Retail Price: </b>", sprintf("<b>%.2f</b>", retail_price), "<br>",
+        "<b>Overall Avg:</b> ", sprintf("<b>%.2f</b>", overall_avg)
+      )
+    })
+    
+    
     
     # Custom title
     custom_title <- paste("<b>The", input$price, "of", input$drug, "in", input$country, "[1990 - 2022]", "</b>")
-
+    
     # Plotly line chart
     plot_ly(data = filtered_data(), x = ~year, type = 'scatter', mode = 'lines',
             y = ~switch(input$price, Retail = retail_price, Wholesale = wholesale_price),
@@ -62,13 +69,17 @@ server <- function(input, output) {
       # Add a line for the overall average
       add_trace(data = overall_average(), x = ~year, y = ~average_price,
                 type = 'scatter', mode = 'lines', name = 'Overall Average',
-                line = list(color = 'black', width = 2)) |> 
+                line = list(color = 'red', width = 2)) |> 
       layout(title = custom_title,
              xaxis = list(title = " "),
-             yaxis = list(title = input$price)) |> 
+             yaxis = list(title = input$price),
+             legend = list(orientation = "h",   # show entries horizontally
+                           xanchor = "center",  # use center of legend as anchor
+                           x = 0.5)) |> 
       config(displayModeBar = FALSE)
   })
 }
+
 
 
 # Run the application 
